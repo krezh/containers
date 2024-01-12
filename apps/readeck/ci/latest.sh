5668407be7b01715f6de2e72617cbab0c8aaad5f
Krezh <krezh@users.noreply.github.com>
2024-01-12 17:55:24 +0100

    add retry

--- apps/readeck/ci/latest.sh
+++ apps/readeck/ci/latest.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-version="$(curl -sX GET "https://codeberg.org/api/v1/repos/readeck/readeck/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
+
+for i in {1..3}; do
+    version="$(curl -sX GET "https://codeberg.org/api/v1/repos/readeck/readeck/releases/latest" | jq --raw-output '.tag_name')"
+    version="${version#*v}"
+    version="${version#*release-}"
+    if [[ -z "$version" ]]; then
+        printf "%s\n" "Failed to get latest version. Retrying in 5 seconds... $i/3"
+        sleep 5
+    else
+        printf "%s" "${version}"
+        break
+    fi
+done
+
