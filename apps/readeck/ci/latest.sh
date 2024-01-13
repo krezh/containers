fdab06824a5236e9f94b50a718ad1ade91933071
Krezh <krezh@users.noreply.github.com>
2024-01-13 14:25:42 +0100

    fix script

--- apps/readeck/ci/latest.sh
+++ apps/readeck/ci/latest.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 for i in {1..3}; do
-    version="$(curl -sX GET "https://codeberg.org/api/v1/repos/readeck/readeck/releases/latest" | jq --raw-output '.tag_name')"
+    version="$(curl -sSL "https://codeberg.org/api/v1/repos/readeck/readeck/releases/latest" | jq --raw-output '.tag_name')"
     version="${version#*v}"
     version="${version#*release-}"
     if [[ -z "$version" ]]; then
-        printf "%s\n" "Failed to get latest version. Retrying in 5 seconds... $i/3"
+        echo -e "Failed to get latest version. Retrying in 5 seconds... $i/3"
         sleep 5
     else
         printf "%s" "${version}"
