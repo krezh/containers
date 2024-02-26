21e125408b7ead6d1f0e40e1d7e6b9453fc929c4
Krezh <krezh@users.noreply.github.com>
2024-02-26 22:58:10 +0100

    Update version to 0.16.0-rc.3 in latest.sh

--- apps/opentofu-runner/ci/latest.sh
+++ apps/opentofu-runner/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #version=$(curl -sX GET "https://api.github.com/repos/flux-iac/tf-controller-old/tags" | jq --raw-output '.[].name' 2>/dev/null | grep -v "\-rc" | head -n 1)
-version="0.15.1"
+version="0.16.0-rc.3"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
