621a6069c15868477d2d43b8b2dfde8c941a3ede
Krezh <krezh@users.noreply.github.com>
2024-12-22 17:38:13 +0100

    fix(valkey) fix versioning

--- apps/valkey/ci/latest.sh
+++ apps/valkey/ci/latest.sh
@@ -4,6 +4,6 @@ regex='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][
 url="https://registry.hub.docker.com/v2/repositories/valkey/valkey/tags?ordering=name&name=$channel"
 version=$(curl -s "$url" | jq --raw-output --arg regex "$regex" --arg s "$channel" '.results[] | select(.name | contains("unstable") | not) | select(.name | contains($s)) | .name | select(test($regex))' 2>/dev/null | head -n1)
 version="${version#*v}"
-version="${version#*release-}"
+version="${version%-"${channel}"}"
 printf "%s" "${version}"
 
