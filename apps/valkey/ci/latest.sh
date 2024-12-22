b32ed3ad4d260ca2340d825fa13a66762b82c466
Krezh <krezh@users.noreply.github.com>
2024-12-22 17:31:52 +0100

    use bookworm for valkey

--- apps/valkey/ci/latest.sh
+++ apps/valkey/ci/latest.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 channel=$1
-# renovate: datasource=docker depName=docker.io/valkey/valkey
-version="8.0.1"
+regex='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+url="https://registry.hub.docker.com/v2/repositories/valkey/valkey/tags?ordering=name&name=$channel"
+version=$(curl -s "$url" | jq --raw-output --arg regex "$regex" --arg s "$channel" '.results[] | select(.name | contains("unstable") | not) | select(.name | contains($s)) | .name | select(test($regex))' 2>/dev/null | head -n1)
+version="${version#*v}"
+version="${version#*release-}"
 printf "%s" "${version}"
 
