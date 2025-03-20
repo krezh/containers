c6c257fb6723afb711f766e0f69c8b7b628bbfb8
Krezh <krezh@users.noreply.github.com>
2025-03-20 11:35:50 +0100

    refactor

--- apps/valkey/ci/latest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-channel=$1
-regex='^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
-url="https://registry.hub.docker.com/v2/repositories/valkey/valkey/tags?ordering=name&name=$channel"
-version=$(curl -s "$url" | jq --raw-output --arg regex "$regex" --arg s "$channel" '.results[] | select(.name | contains("unstable") | not) | select(.name | contains($s)) | .name | select(test($regex))' 2>/dev/null | head -n1)
-version="${version#*v}"
-version="${version%-"${channel}"}"
-printf "%s" "${version}"
-
