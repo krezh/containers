e11f069495dd8be88837ee97f29d9fe16c04127e
Krezh <krezh@users.noreply.github.com>
2025-04-12 01:02:57 +0200

    fix(tests): reduce GOSS retry timeout from 600s to 60s and remove obsolete tests.yaml

--- .github/actions/container-tests/run.sh
+++ .github/actions/container-tests/run.sh
@@ -8,7 +8,7 @@ if [[ -x "$(command -v container-structure-test)" ]]; then
     container-structure-test test --image "${IMAGE}" --config "./apps/${APP}/tests.yaml"
 elif [[ -x "$(command -v goss)" && -x "$(command -v dgoss)" ]]; then
     export GOSS_FILE="./apps/${APP}/tests.yaml"
-    export GOSS_OPTS="--retry-timeout 600s --sleep 1s"
+    export GOSS_OPTS="--retry-timeout 60s --sleep 1s"
     dgoss run "${IMAGE}"
 else
     echo "No testing tool found. Exiting."
