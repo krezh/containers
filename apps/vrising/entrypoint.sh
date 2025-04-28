a1009229274cd0af73f5084c9072efc343e72b94
Krezh <krezh@users.noreply.github.com>
2025-04-28 21:54:59 +0200

    test

--- apps/vrising/entrypoint.sh
+++ apps/vrising/entrypoint.sh
@@ -93,16 +93,8 @@ run() {
     -persistentDataPath "$persistentDataPath" \
     -logFile "$persistentDataPath/$logfile" \
     -serverName "${SERVER_NAME}" \
-    -description "${DESCRIPTION}" \
     -gamePort "${GAME_PORT}" \
     -queryPort "${QUERY_PORT}" \
-    -bindAddress "${BIND_ADDRESS}" \
-    -hideIpAddress "${HIDE_IP}" \
-    -lowerFPSWhenEmpty "${LOWER_FPS_EMPTY}" \
-    -password "${SERVER_PASSWORD}" \
-    -secure "${SECURE}" \
-    -listOnEOS "${EOS_LIST}" \
-    -listOnSteam "${STEAM_LIST}" \
     -saveName "${SAVE_NAME}" 2>&1
 }
 
