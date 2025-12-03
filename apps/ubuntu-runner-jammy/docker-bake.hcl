af6849f8f0662c7a21b92fe7fcbabe36c1f2a975
Krezh <krezh@users.noreply.github.com>
2025-12-03 06:24:15 +0100

    feat: add ubuntu-runner-jammy and ubuntu-runner-noble containerDisk images

---
+++ apps/ubuntu-runner-jammy/docker-bake.hcl
@@ -0,0 +1,48 @@
+target "docker-metadata-action" {}
+
+variable "APP" {
+  default = "ubuntu-runner-jammy"
+}
+
+variable "VERSION" {
+  default = "22.04"
+}
+
+variable "SOURCE" {
+  default = "https://github.com/krezh/containers"
+}
+
+group "default" {
+  targets = ["image-local"]
+}
+
+variable "RUNNER_VERSION" {
+  // renovate: datasource=github-releases depName=actions/runner
+  default = "2.321.0"
+}
+
+target "image" {
+  inherits = ["docker-metadata-action"]
+  args = {
+    UBUNTU_VERSION = "${VERSION}"
+    RUNNER_VERSION = "${RUNNER_VERSION}"
+  }
+  labels = {
+    "org.opencontainers.image.source" = "${SOURCE}"
+  }
+}
+
+target "image-local" {
+  inherits = ["image"]
+  output = ["type=docker"]
+  tags = ["${APP}:${VERSION}"]
+  # Enable security.insecure for virt-customize to work
+  allow = ["security.insecure"]
+}
+
+target "image-all" {
+  inherits = ["image"]
+  platforms = [
+    "linux/amd64"
+  ]
+}
---
+++ apps/ubuntu-runner-noble/container_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/krezh/containers/testhelpers"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+)
+
+func Test(t *testing.T) {
+	ctx := context.Background()
+	image := testhelpers.GetTestImage("ghcr.io/krezh/ubuntu-runner-noble:rolling")
+
+	// ContainerDisk images are FROM scratch and only contain the disk image file at /disk/disk.img
+	// We verify the image exists and can be pulled, then inspect its layers
+	c, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image: image,
+			Cmd:   []string{"/disk/disk.img"}, // Dummy command that won't actually run
+		},
+		Started: false, // Don't try to start the container
+	})
+	require.NoError(t, err)
+	defer func() {
+		if c != nil {
+			_ = c.Terminate(ctx)
+		}
+	}()
+
+	// Verify the container was created successfully
+	// The actual disk image will be validated when used with KubeVirt
+	require.NotNil(t, c)
+}
