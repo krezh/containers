0bd282e3d8312a8c7ce38b3afc50386d77f0986d
Krezh <krezh@users.noreply.github.com>
2026-02-04 16:12:37 +0100

    add timescaledb

--- apps/cdpg-vchord-18/container_test.go
+++ apps/cdpg-vchord-18/container_test.go
@@ -10,5 +10,9 @@ import (
 func Test(t *testing.T) {
 	ctx := context.Background()
 	image := testhelpers.GetTestImage("ghcr.io/krezh/cdpg-vchord:rolling")
-	testhelpers.TestFileExists(t, ctx, image, "/usr/pgsql-16/lib/vchord.so", nil)
+
+	testhelpers.TestFileExists(t, ctx, image, "/usr/pgsql-18/lib/vchord.so", nil)
+	testhelpers.TestFileExists(t, ctx, image, "/usr/pgsql-18/lib/timescaledb.so", nil)
+	testhelpers.TestCommandSucceeds(t, ctx, image, nil, "sh", "-c", "ls /usr/pgsql-18/lib/timescaledb-tsl-*.so")
+	testhelpers.TestCommandSucceeds(t, ctx, image, nil, "sh", "-c", "test $(id -u) -ne 0")
 }
