679feec272d211712bd5bf09e82942071c6c4312
Krezh <krezh@users.noreply.github.com>
2026-05-01 13:16:41 +0200

    install nix at runtime

--- apps/renovate-nix/container_test.go
+++ apps/renovate-nix/container_test.go
@@ -11,6 +11,10 @@ func Test(t *testing.T) {
 	ctx := context.Background()
 	image := testhelpers.GetTestImage("ghcr.io/krezh/renovate-nix:rolling")
 
-	testhelpers.TestFileExists(t, ctx, image, "/home/ubuntu/.nix-profile/bin/nix", nil)
-	testhelpers.TestCommandSucceeds(t, ctx, image, nil, "sh", "-c", "/home/ubuntu/.nix-profile/bin/nix --version")
+	// Test that entrypoint script exists and is executable
+	testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/entrypoint.sh", nil)
+
+	// Test that the entrypoint installs Nix and it works
+	// The entrypoint will install Nix on first run, then verify it's available
+	testhelpers.TestCommandSucceeds(t, ctx, image, nil, "/usr/local/bin/entrypoint.sh", "--version")
 }
